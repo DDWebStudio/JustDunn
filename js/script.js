e3c291dcefed531b5ff7c981152021ee0ae14417
VitaliyPr <vitaliyprudnyk@gmail.com>
2020-07-18 18:45:41 +0300

    desctop-change

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+function link() {
+  $('a[href^="#"], *[data-href^="#"]').on('click', function (e) {
+    e.preventDefault();
+    var t = 100;
+    var d = $(this).attr('data-href') ? $(this).attr('data-href') : $(this).attr('href');
+    $('html,body').stop().animate({
+      scrollTop: $(d).offset().top
+    }, t);
+  });
+}
+
 jQuery(document).ready(function () {
+  link();
   new WOW().init();
 });
